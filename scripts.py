f063320e41bcbb4ed5413d9b3873bb29338ad15a
fastfir <95661133+fastfir@users.noreply.github.com>
2022-10-05 08:39:48 -0700

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,25 @@
+import os
+drt = os.path.expanduser("~")
+print(drt)
+def site(site):
+    os.chdir(drt + "/Anubis")
+    os.system("anubis -t " + site)
+    os.chdir(drt + "/nmap")
+    os.system("/nmap -oX /home/fastfir/nmap.xml -Pn " + site)
+def user(user):
+    os.chdir(drt + "/sherlock/sherlock")
+    os.system("python3 sherlock.py " + user)
+    os.chdir(drt + "/nexfil")
+    os.system("python3 nexfil.py " + user)
+    os.chdir(drt + "/maigret")
+    os.system("go run maigret.go " + user)
+    os.chdir(drt + "/social-analyzer/pip-social-analyzer/social-analyzer")
+    os.system("python3 -m social-analyzer --username " + user + " --metadata --top100")
+def email(email):
+    os.chdir(drt + "/holehe")
+    os.system("holehe " + email)
+    os.chdir(drt + "/theHarvester")
+    domain = email.split("@",1)
+    domain = domain[1]
+    os.system("python3 theHarvester.py -d " + domain + " -b all")
+
