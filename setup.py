2751d7710f115055a0f9d55b8f0ffeda052ea936
fastfir <95661133+fastfir@users.noreply.github.com>
2022-10-05 23:47:30 -0700

    Added Infoga and fixed a small issue

--- setup.py
+++ setup.py
@@ -3,26 +3,32 @@ direc = input("Enter the directory that you want to install to: ")
 try:
     os.chdir(direc + "/sherlock/sherlock")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/sherlock-project/sherlock")
 try:
     os.chdir(direc + "/nexfil")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/thewhiteh4t/nexfil")
 try:
     os.chdir(direc + "/maigret")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/torerobo/maigret")
 try:
     os.chdir(direc + "/social-analyzer")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/qeeqbox/social-analyzer")
 try:
     os.chdir(direc + "/Anubis")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/jonluca/Anubis")
 try:
     os.chdir(direc + "/nmap")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/nmap/nmap")
     os.system("./configure")
     os.system("make")
@@ -30,8 +36,12 @@ except:
 try:
     os.chdir(direc + "/holehe")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/megadose/holehe")
 try:
     os.chdir(direc + "/theHarvester")
 except:
+    os.chdir(direc)
     os.system("git clone https://github.com/laramies/theHarvester")
+try:
+    os.chdir(direc + "/Infoga")
